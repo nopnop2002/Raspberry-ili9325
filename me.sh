8ed23d1e610b0a03c7cef8ea741a732af89a6128
nopnop2002 <hau26280@tree.odn.ne.jp>
2019-06-11 00:21:37 +0900

    Change Background Color

--- me.sh
+++ me.sh
@@ -27,38 +27,42 @@ _echo "DrawUTF8String,G24,170,300,wlan0 IP Address,${color}"
 #_echo "UnsetFontUnderLine"
 IpAddr=`ifconfig wlan0 |grep "inet " |awk {'print $2'}`
 if [ -n "$IpAddr" ]; then
+  _echo "SetFontUnderLine,${color}"
   _echo "DrawUTF8String,G24,150,300,${IpAddr},${color}"
+  _echo "UnsetFontUnderLine"
 else
   _echo "DrawUTF8String,G24,150,300,<NONE>,${color}"
 fi
 
 #Magenta(0xff0090)
 color=`./rgb2color 0xff0090`
-_echo "DrawFillRect,135,305,145,315,${color}"
-#_echo "SetFontUnderLine,${color}"
-_echo "DrawUTF8String,G24,130,300,eth0 IP Address,${color}"
-#_echo "UnsetFontUnderLine"
+_echo "DrawFillRect,130,305,140,315,${color}"
+_echo "DrawUTF8String,G24,125,300,eth0 IP Address,${color}"
 IpAddr=`ifconfig eth0 |grep "inet " |awk {'print $2'}`
 if [ -n "$IpAddr" ]; then
-  _echo "DrawUTF8String,G24,110,300,${IpAddr},${color}"
+  _echo "SetFontUnderLine,${color}"
+  _echo "DrawUTF8String,G24,105,300,${IpAddr},${color}"
+  _echo "UnsetFontUnderLine"
 else
-  _echo "DrawUTF8String,G24,110,300,<NONE>,${color}"
+  _echo "DrawUTF8String,G24,105,300,<NONE>,${color}"
 fi
 
 #Orange(0xff5c00)
 color=`./rgb2color 0xff5c00`
-_echo "DrawFillRect,95,305,105,315,${color}"
-#_echo "SetFontUnderLine,${color}"
-_echo "DrawUTF8String,G24,90,300,kernel release,${color}"
-#_echo "UnsetFontUnderLine"
+_echo "DrawFillRect,85,305,95,315,${color}"
+_echo "DrawUTF8String,G24,80,300,kernel release,${color}"
 Release=`uname -r`
-_echo "DrawUTF8String,G24,70,300,${Release},${color}"
+_echo "SetFontUnderLine,${color}"
+_echo "DrawUTF8String,G24,60,300,${Release},${color}"
+_echo "UnsetFontUnderLine"
 
 #Lime Green(0x00ff00)
 color=`./rgb2color 0x00ff00`
-_echo "DrawFillRect,55,305,65,315,${color}"
-_echo "DrawUTF8String,G24,50,300,CPU Temp.,${color}"
+_echo "DrawFillRect,40,305,50,315,${color}"
+_echo "DrawUTF8String,G24,35,300,CPU Temp.,${color}"
 temp=`vcgencmd measure_temp | cut -d= -f2`
-_echo "DrawUTF8String,G24,30,300,${temp},${color}"
+_echo "SetFontUnderLine,${color}"
+_echo "DrawUTF8String,G24,15,300,${temp},${color}"
+_echo "UnsetFontUnderLine"
 
 ./draw $DrawFile > /dev/null
